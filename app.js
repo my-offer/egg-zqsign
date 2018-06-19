ca50b5d7f01728e2ee7dddaf0b2dc28dabf85914
liuyanjie <lyj8888888888@gmail.com>
2018-06-19 13:27:20 +0800

    fix(app): assign config

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ module.exports = app => {
   app.addSingleton('zqSign', async function createZQSign(config, app) {
     assert(config.url && config.zqid && config.privateKey && config.publicKey);
     app.coreLogger.info(`[egg-zqsign]: ${config.url} ${config.zqid}`)
-    return new ZQSign(config.url, config.zqid, config.privateKey, config.publicKey, {alg: 'RSA-SHA1'})
+    return new ZQSign(config.url, config.zqid, config.privateKey, config.publicKey, Object.assign({
+      alg: 'RSA-SHA1'
+    }, config))
   })
 }
